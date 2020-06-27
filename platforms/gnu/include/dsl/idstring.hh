892b1dbb9e3c43b0a529ca234b42c140e225bad6
Campbell Suter <znix@znix.xyz>
2020-06-27 15:17:23 +1200

    Linux: add struct definition for dsl::ResourceID

--- platforms/gnu/include/dsl/idstring.hh
+++ platforms/gnu/include/dsl/idstring.hh
@@ -11,6 +11,12 @@ namespace dsl
 	public:
 		idstring_t value;
 	};
+
+	struct ResourceID
+	{
+		idstring_t type = 0;
+		idstring_t name = 0;
+	};
 };
 
 /* vim: set ts=3 softtabstop=0 sw=3 expandtab: */

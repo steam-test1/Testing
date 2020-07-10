21b03c907d35ee75d1dcdac40820d6f6ecec3ade
Campbell Suter <znix@znix.xyz>
2020-07-10 19:08:47 +1200

    Rename the 'gnu' platform to 'linux'

--- src/lua.h
+++ src/lua.h
@@ -4,10 +4,8 @@
 
 #ifdef _WIN32
 #include "../platforms/w32/signatures/sigdef.h"
-#elif __GNUC__
-#include "../platforms/gnu/include/lua.hh"
 #else
-#error Unknown platform - either _WIN32 or __GNUC__ must be defined
+#include "../platforms/linux/include/lua.hh"
 #endif
 
 inline uint64_t luaX_toidstring(lua_State *L, int index)

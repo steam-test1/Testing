246c62c0dcde23788f9d1e88b0269346a93ada7c
Campbell Suter <znix@znix.xyz>
2021-05-23 10:27:22 +1200

    Add Environment.mod_directory_at_depth

--- src/tweaker/wren_environment.cpp
+++ src/tweaker/wren_environment.cpp
@@ -15,17 +15,44 @@ extern "C"
 #include "../../lib/wren/src/vm/wren_vm.h"
 }
 
-void get_mod_directory(WrenVM* vm)
+void get_mod_directory_impl(WrenVM* vm, int depth)
 {
 	// Poke around in Wren's internals to do this - not ideal but hey it works (for now...)
 	ObjFiber* fibre = vm->fiber;
 
-	ObjFn* caller = fibre->frames[fibre->numFrames - 1].closure->fn;
+	int numFrames = fibre->numFrames;
+	int frameId = numFrames - 1 - depth;
+	if (frameId >= numFrames || frameId < 0)
+	{
+		char buff[128];
+		snprintf(buff, sizeof(buff), "Failed to find mod directory, bad frame ID %d count %d", frameId, numFrames);
+		wrenSetSlotString(vm, 0, buff);
+		wrenAbortFiber(vm, 0);
+		return;
+	}
+
+	ObjFn* caller = fibre->frames[frameId].closure->fn;
 	std::string callerModule = caller->module->name->value;
 
 	// Chop it down to the mod name
 	std::string mod = callerModule;
-	mod.erase(mod.find_first_of("/"));
+	int stroke_index = mod.find_first_of('/');
+
+	// If there's no stroke ('/') character, it's something like the meta package
+	// We can afford to be a bit intolerant of invalid inputs here, since this is only designed for
+	// namespacing stuff, and they should never just be called directly through meta or anything - and
+	// if they did it'll likely result in unintended behaviour that will be hard to diagnose.
+	if (stroke_index == -1)
+	{
+		char buff[128];
+		snprintf(buff, sizeof(buff), "Non-mod-directory caller path '%s'", callerModule.c_str());
+		wrenSetSlotString(vm, 0, buff);
+		wrenAbortFiber(vm, 0);
+		return;
+	}
+
+	// Erase everything after the stroke to leave us with the first part of the module path
+	mod.erase(stroke_index);
 
 	// Convert it to the mod directory
 	// If we let people change the mod name distinct from it's directory, we'll have to
@@ -33,6 +60,28 @@ void get_mod_directory(WrenVM* vm)
 	std::string dir = "mods/" + mod;
 
 	wrenSetSlotString(vm, 0, dir.c_str());
+}
+
+void get_mod_directory(WrenVM* vm)
+{
+	get_mod_directory_impl(vm, 0);
+}
+
+void get_mod_directory_at_depth(WrenVM* vm)
+{
+	int type = wrenGetSlotType(vm, 1);
+	if (type != WREN_TYPE_NUM)
+	{
+		char buff[128];
+		snprintf(buff, sizeof(buff), "Failed to find mod directory depth, invalid non-number type id=%d", type);
+		wrenSetSlotString(vm, 0, buff);
+		wrenAbortFiber(vm, 0);
+		return;
+	}
+
+	int depth = (int)wrenGetSlotDouble(vm, 1);
+
+	get_mod_directory_impl(vm, depth);
 }
 
 WrenForeignMethodFn pd2hook::tweaker::wren_env::bind_wren_env_method(WrenVM* vm, const char* module,
@@ -46,6 +95,10 @@ WrenForeignMethodFn pd2hook::tweaker::wren_env::bind_wren_env_method(WrenVM* vm,
 			if (isStatic && strcmp(signature, "mod_directory") == 0)
 			{
 				return &get_mod_directory;
+			}
+			else if (isStatic && strcmp(signature, "mod_directory_at_depth(_)") == 0)
+			{
+				return &get_mod_directory_at_depth;
 			}
 		}
 	}
--- wren/Environment_001.wren
+++ wren/Environment_001.wren
@@ -5,4 +5,11 @@ class Environment {
     // Note this is queried with *native magic*, so unlike ModPath in Lua it depends
     // on who's calling it - thus you don't have to cache it's result value.
     foreign static mod_directory
+
+    // When depth is 0 this is the same as mod_directory. When depth is 1 this finds the
+    // mod directory of whatever's calling the function calling this, and so on.
+    // This is particularly useful inside the basemod to separate stuff by mod, but care
+    // should be used when using it as it can lead to non-obvious results for code a few
+    // layers up in the callstack.
+    foreign static mod_directory_at_depth(depth)
 }
